7c604a8ffeca85bf528bf603e50e4d7abd7e8f38
Arjun Adhikary <reasonworld12345@gmail.com>
2020-10-19 12:50:49 +0545

    minor bugs fixed

--- app/src/main/java/com/arjun/weather/ItemsHourly.java
+++ app/src/main/java/com/arjun/weather/ItemsHourly.java
@@ -1,9 +1,8 @@
 package com.arjun.weather;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
+import android.os.Looper;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,13 +11,14 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.arjun.weather.Model.ItemHourly;
 import com.arjun.weather.RecyclerView.Adapter;
+import com.arjun.weather.utils.FormatDate;
 import com.google.gson.Gson;
-
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 
 public class ItemsHourly extends AppCompatActivity {
@@ -34,17 +34,19 @@ public class ItemsHourly extends AppCompatActivity {
         Type type = new TypeToken<List<ItemHourly>>() {
         }.getType();
         listPrivate = new Gson().fromJson(getIntent().getStringExtra("private_list"), type);
+        Objects.requireNonNull(getSupportActionBar()).setElevation(0);
+        getSupportActionBar().setTitle(new FormatDate().onlyDay(listPrivate.get(0).getDtTxt()) + "'s Weather");
 
         setRecyclerView();
-        new Handler().postDelayed((Runnable) () -> {
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
             adapter.isShimmer = false;
             adapter.notifyDataSetChanged();
-        },0,2000);
+        }, 0, 3000);
     }
 
     private void setRecyclerView() {
         recyclerView = findViewById(R.id.hourly_main);
-        adapter = new Adapter(listPrivate,this);
+        adapter = new Adapter(listPrivate, this);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
